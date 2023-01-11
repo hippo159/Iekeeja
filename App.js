da3ecc770b825bfe1cd6a2a92e8cbeb8f3fb9685
hippo159 <46355784+hippo159@users.noreply.github.com>
2023-01-11 15:35:16 +0100

    feat: styling update

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import FurnitureItem from './components/FurnitureItem';
 
 export default function App() {
   return (
+    <ScrollView style={styles.Furniturecontainer}>
     <View style={styles.container}>
+      <FurnitureItem title ="Development 5" subTitle="YP2908 / 2022"/>
       <FurnitureItem title ="Development 5" subTitle="YP2908 / 2022"/>
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    flexGrow: 0,
+    flexShrink:0 ,
+    justifyContent: "space-evenly",
+  },
+  Furniturecontainer: {
+    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+        
+    
   },
 });

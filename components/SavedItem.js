55b7031d68568f23c3897a1d76a6d4b7b81cad0a
hippo159 <46355784+hippo159@users.noreply.github.com>
2023-01-15 14:52:59 +0100

    feat: display test text in saved

--- components/SavedItem.js
+++ components/SavedItem.js
@@ -1,10 +1,59 @@
 import { useState } from 'react';
-//import FurnitureItem from './FurnitureItem';
+import{ StyleSheet, Text, View, Image, ScrollView, StatusBar, } from 'react-native';
+import { Button, IconButton } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
-const savedItems = [];
-const SavedItem = (itemId) => {
-    savedItems.push(itemId);
-    console.log(savedItems);
+console.log("SavedItem.js loaded")
+const SavedItem = (props) => {
+const navigation = useNavigation();
+return (
+<View style={styles.tile}>
+<Image style={styles.tileImage} source={{uri: props.image}} />
+    <View style={styles.tileDetails}>
+    <Text style={styles.tileTitle}>{props.title}</Text>
+    <Text style={styles.tileSubTitle}>{props.tileSubTitle}</Text>
+    </View>    
+    <View style={styles.tileButtons}>
    
+    </View>
+</View>
+);
 }
+const styles = StyleSheet.create({
+tileButtons:{
+    
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    padding: 5,
+},
+
+tile:{
+    height: 'auto',
+    borderColor: "#B4B8DA",
+    borderBottomWidth: 1,
+    borderRadius: 5,
+    flexBasis: "90%",
+    flexDirection: "column",
+    marginVertical: '2%',
+    
+    
+  },
+  tileTitle:{
+    fontSize: 26,
+    color:"#6b4faa",
+    fontWeight: "800",
+    paddingBottom: 10,
+  },
+  tileSubTitle:{
+    fontSize: 16,
+  },
+  tileImage:{
+    width: "100%",
+    height: 180,
+    borderRadius: 5,
+  },
+  tileDetails:{
+    padding: 6,
+  }
+});
 export default SavedItem;
--- screens/SavedItemsScreen.js
+++ screens/SavedItemsScreen.js
@@ -2,30 +2,71 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView, StyleSheet, Text, View, FlatList, } from 'react-native';
 import SavedItem from '../components/SavedItem';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { BottomNavigation, Provider as PaperProvider } from 'react-native-paper';
 
 
 const SavedItemsScreen = (props) => {
-    return (
-        <PaperProvider>
-    <View style={styles.Furniturecontainer}>
+    //get posts from api
+    const [posts, setPosts] = useState([]);
+    const getPostById = async (id) => {
+      try{
+        const response = await fetch(`https://www.vandycklukas.be/wp-json/wp/v2/posts?id=${id}`);
+        const json = await response.json();
+        setPosts((prevPosts) => [...prevPosts, json]);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
     
-            <Text>iets anders</Text>
-          </View>
-          </PaperProvider>
-        );
-            }
-    const styles = StyleSheet.create({
-        container: {
-            flex:1,
-            backgroundColor: '#fff',
-            padding: 20,
-        },
-        Furniturecontainer: {
-          
-          flex: 1,
-          flexWrap: "wrap",
-          
-        },
-      });
+    const getPostsFromAsyncStorage = async () => {
+      try {
+        const ids = await AsyncStorage.getItem('savedItems');
+        const idArray = JSON.parse(ids);
+        idArray.forEach(id => getPostById(id));
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    
+    useEffect(() => {
+      getPostsFromAsyncStorage();
+      
+    }, []);
+return (
+  <PaperProvider>
+<View style={styles.Furniturecontainer}>
+
+      <FlatList
+      style={styles.container}
+      data={posts}
+      keyExtractor={item => item.id}
+      renderItem={({item, index}) => 
+      (<SavedItem 
+        //get image from wordpress api
+        //image={item.featured_media_urls.large[0]}
+        //id={item.id}
+        title= "test title"//{item.title.rendered} 
+        subTitle="test sub"//{item.excerpt.rendered}
+        />)}
+      />
+    </View>
+    </PaperProvider>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+      flex:1,
+      backgroundColor: '#fff',
+      padding: 20,
+  },
+  Furniturecontainer: {
+    
+    flex: 1,
+    flexWrap: "wrap",
+    
+  },
+});
       export default SavedItemsScreen;
